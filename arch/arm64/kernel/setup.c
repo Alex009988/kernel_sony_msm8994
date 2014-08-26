822ee0516408e56f76bcaa07f100aa92ba5dde39
Jon Masters <jcm@redhat.com>
2014-08-26 21:23:38 +0100

    arm64/setup: Move unmask of async interrupts after possible earlycon setup
    The kernel wants to enable reporting of asynchronous interrupts (i.e.
    System Errors) as early as possible. But if this happens too early then
    any pending System Error on initial entry into the kernel may never be
    reported where a user can see it. This situation will occur if the kernel
    is configured with CONFIG_PANIC_ON_OOPS set and (default or command line)
    enabled, in which case the kernel will panic as intended, however the
    associated logging messages indicating this failure condition will remain
    only in the kernel ring buffer and never be flushed out to the (not yet
    configured) console. Therefore, this patch moves the enabling of
    asynchronous interrupts during early setup to as early as reasonable,
    but after parsing any possible earlycon parameters setting up earlycon.
    
    Signed-off-by: Jon Masters <jcm@redhat.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/kernel/setup.c
+++ arch/arm64/kernel/setup.c
@@ -427,11 +427,6 @@ early_param("warmboot", sony_param_warmboot);
 
 void __init setup_arch(char **cmdline_p)
 {
-	/*
-	 * Unmask asynchronous aborts early to catch possible system errors.
-	 */
-	local_async_enable();
-
 	setup_processor();
 
 	setup_machine_fdt(__fdt_pointer);
@@ -447,6 +442,12 @@ void __init setup_arch(char **cmdline_p)
 	early_ioremap_init();
 
 	parse_early_param();
+
+	/*
+	 *  Unmask asynchronous aborts after bringing up possible earlycon.
+	 * (Report possible System Errors once we can report this occurred)
+	 */
+	local_async_enable();
 
 	efi_init();
 	arm64_memblock_init();
