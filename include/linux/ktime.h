dc3addfa14debd8e4a1e5a44d468197d30474d3b
sabar <sabar.pakpahan@gmail.com>
2021-08-31 11:51:04 +0700

    ktime: add ms_to_ktime() and ktime_add_ms() helpers
    Add two ktime helper functions that i) convert a given msec value to
    a ktime structure and ii) that adds a msec value to a ktime structure.
    
    Signed-off-by: Daniel Borkmann <dborkman@redhat.com>
    Acked-by: Vlad Yasevich <vyasevich@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Joe Maples <joe@frap129.org>

--- include/linux/ktime.h
+++ include/linux/ktime.h
@@ -216,6 +216,11 @@ static inline ktime_t ktime_add_us(const ktime_t kt, const u64 usec)
 	return ktime_add_ns(kt, usec * NSEC_PER_USEC);
 }
 
+static inline ktime_t ktime_add_ms(const ktime_t kt, const u64 msec)
+{
+	return ktime_add_ns(kt, msec * NSEC_PER_MSEC);
+}
+
 static inline ktime_t ktime_sub_us(const ktime_t kt, const u64 usec)
 {
 	return ktime_sub_ns(kt, usec * NSEC_PER_USEC);
@@ -273,6 +278,7 @@ static inline __must_check bool ktime_to_timespec64_cond(const ktime_t kt,
 static inline ktime_t ns_to_ktime(u64 ns)
 {
 	static const ktime_t ktime_zero = { .tv64 = 0 };
+
 	return ktime_add_ns(ktime_zero, ns);
 }
 
